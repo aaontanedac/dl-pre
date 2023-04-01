f382928addee4d4b994fa50f60c4c9f1e58f419b
Adrian Ontaneda <99149093+aaontanedac@users.noreply.github.com>
2023-04-01 09:55:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://raw.githubusercontent.com/aaontanedac/dl-pre/main/script.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=githubusercontent.com
+// @grant        none
+// ==/UserScript==
+
 var deck = document.getElementsByClassName('deck-container svelte-z2tetq')[0];
 var cartas = deck.getElementsByClassName('card svelte-1h71uu4');
 var allrarity = [];
