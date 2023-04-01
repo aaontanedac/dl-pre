a96b635d2a2d69c5398766e6fb58347040edbf93
Adrian Ontaneda <99149093+aaontanedac@users.noreply.github.com>
2023-04-01 16:04:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Formato ZC Duel Links
-// @version     1.0.1
+// @version     1.0.2
 // @description Añade un contador de precios para duelinks meta
 // @author      @aaontanedac
 // @copyright   2023
@@ -8,45 +8,45 @@
 // @namespace   zc
 // @homepageURL https://gist.github.com/aaontanedac
 // @match       https://www.duellinksmeta.com/deck-tester#build
-// @downloadURL https://gist.githubusercontent.com/aaontanedac/ac015a44be502489f85394eda9216ba3/raw/f3b83ee7296857f2724874bb0941c73f97b3b4d4/dl-format-ext.js
-// @updateURL https://gist.githubusercontent.com/aaontanedac/ac015a44be502489f85394eda9216ba3/raw/f3b83ee7296857f2724874bb0941c73f97b3b4d4/dl-format-ext.js
+// @downloadURL https://raw.githubusercontent.com/aaontanedac/dl-pre/main/script.js
+// @updateURL   https://raw.githubusercontent.com/aaontanedac/dl-pre/main/script.js
 
 // ==/UserScript==
 
 
 function calcular_valor(){
-var deck = document.getElementsByClassName('deck-container svelte-z2tetq')[0];
-var cartas = deck.getElementsByClassName('card svelte-1h71uu4');
-var allrarity = [];
-var valor = 0;
-
-console.log(cartas.lenght);
-for (var carta of cartas) {
-  try {
-    var rareza = carta
-      .getElementsByClassName('rarity-image')[0]
-      .getElementsByTagName('img')[0];
-    allrarity.push(rareza.getAttribute('alt'));
-  } catch (error) {}
-}
-
-for (var rar of allrarity) {
-  switch (rar) {
-    case 'N Rarity':
-      valor += 25;
-      break;
-    case 'R Rarity':
-      valor += 50;
-      break;
-    case 'SR Rarity':
-      valor += 100;
-      break;
-    case 'UR Rarity':
-      valor += 200;
-      break;
-    default:
-      valor += 0;                         
-      break;
+  var deck = document.getElementsByClassName('deck-container svelte-z2tetq')[0];
+  var cartas = deck.getElementsByClassName('card svelte-1h71uu4');
+  var allrarity = [];
+  var valor = 0;
+  
+  console.log(cartas.lenght);
+  for (var carta of cartas) {
+    try {
+      var rareza = carta
+        .getElementsByClassName('rarity-image')[0]
+        .getElementsByTagName('img')[0];
+      allrarity.push(rareza.getAttribute('alt'));
+    } catch (error) {}
   }
-}
+
+  for (var rar of allrarity) {
+    switch (rar) {
+      case 'N Rarity':
+        valor += 25;
+        break;
+      case 'R Rarity':
+        valor += 50;
+        break;
+      case 'SR Rarity':
+        valor += 100;
+        break;
+      case 'UR Rarity':
+        valor += 200;
+        break;
+      default:
+        valor += 0;                         
+        break;
+    }
+  }
 }
