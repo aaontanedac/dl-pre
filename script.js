66b2fdc40e74609ff18809ae42e4269ca1ab00e7
Adrian Ontaneda <99149093+aaontanedac@users.noreply.github.com>
2023-04-02 00:13:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Formato ZC Duel Links
-// @version     1.0.6
+// @version     1.0.7
 // @description Añade un contador de precios para duelinks meta
 // @author      @aaontanedac
 // @copyright   2023
